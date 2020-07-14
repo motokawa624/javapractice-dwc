c7303f5ececb049d784ea5f7a751294d8d96ba0f
motokawa624 <61586261+motokawa624@users.noreply.github.com>
2020-07-14 15:54:44 +0900

    [FINISH]java4章変数を学ぼう

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "モトカワ";
+		System.out.println(str1);
+		
+		str1 = "オロナミンC";
+		System.out.println(str1);
+		
+		final int LUCCKY_NUMBER = 7;
+		System.out.println(LUCCKY_NUMBER);
+	}
+}
