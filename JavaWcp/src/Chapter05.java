d19280b969eec88c0ac7be43b9c4f8a481fc43d2
motokawa624 <61586261+motokawa624@users.noreply.github.com>
2020-07-14 16:44:04 +0900

    [FINISH]javaコンテンツ【基礎編】5章[型の基本を学ぼう]

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,28 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123L;
+		double double1 = 123.45;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Integer i = 100;
+		Float f = 1.4142f;
+		Character c = 'う';
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(c);
+		
+		
+		
+		int[] array1 = new int[5];
+		array1[0] = 1;
+		array1[1] = 2;
+		array1[2] = 3;
+		array1[3] = 4;
+		array1[4] = 5;
+		System.out.println(array1[2]);
+	}
+}
