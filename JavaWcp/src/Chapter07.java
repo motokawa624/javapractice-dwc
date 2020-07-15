14738dfc7a360a227100cef2c1de465864769b14
motokawa624 <61586261+motokawa624@users.noreply.github.com>
2020-07-15 16:05:55 +0900

    [FINISH]JAVAコンテンツ【基礎編】7章「条件分岐を学ぼう」

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,13 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int LunchMoney = 700;
+		if (LunchMoney >= 800) {
+			System.out.println("チャーシュー麺が食べられます");
+		}else if (LunchMoney > 600) {
+			System.out.println("ラーメンが食べられます");
+		}else {
+			System.out.println("どのラーメンも食べられません");
+		}
+	}
+}
